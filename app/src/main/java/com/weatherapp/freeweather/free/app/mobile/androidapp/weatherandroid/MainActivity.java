0c7670dd9076075a6e5129ec1a5f36106a461718
azzilian <crusotdeath@gmail.com>
2024-08-25 13:36:27 +0300

    Refactor - margins and constrains - added test icons

--- app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroid/MainActivity.java
+++ app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroid/MainActivity.java
@@ -2,11 +2,13 @@ package com.weatherapp.freeweather.free.app.mobile.androidapp.weatherandroid;
 
 import android.os.Bundle;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.weatherapp.freeweather.free.app.mobile.androidapp.weatherandroid.api.CityDataloader;
 import com.weatherapp.freeweather.free.app.mobile.androidapp.weatherandroid.api.CountryDataLoader;
+import com.weatherapp.freeweather.free.app.mobile.androidapp.weatherandroid.mapper.WeatherConditions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WeatherConditions.loadConditions(this);
+        ImageView weatherIcon = findViewById(R.id.weather_today_img);
+        int weatherCode = 113;
+        int iconResId = WeatherConditions.getIconForCode(weatherCode);
+
+        if (iconResId != R.drawable.weather_default) {
+            weatherIcon.setImageResource(iconResId);
+        } else {
+            System.out.println("Using default icon");
+            weatherIcon.setImageResource(R.drawable.weather_default);
+        }
 
         countriesSpinner = findViewById(R.id.countries);
         citiesSpinner = findViewById(R.id.cities);
---
+++ app/src/main/java/com/weatherapp/freeweather/free/app/mobile/androidapp/weatherandroid/api/WeatherDataLoader.java
@@ -0,0 +1,4 @@
+package com.weatherapp.freeweather.free.app.mobile.androidapp.weatherandroid.api;
+
+public class WeatherDataLoader {
+}
